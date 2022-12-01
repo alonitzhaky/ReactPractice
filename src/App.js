f8d0ce5bdfb1f4cf97065f4160997d2de47bff8b
alon.itzhaky <alon.itzhaky@gmail.com>
2022-12-01 22:53:55 +0200

    Commit 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Navbar from './components/Navbar';
 import { Outlet } from 'react-router-dom'
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const notify = () => toast("Wow so easy!");
   return (
     <div>
       <Navbar/>
       <Outlet/>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer/>
     </div>
   );
 };
